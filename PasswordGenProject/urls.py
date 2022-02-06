d5f26a3cde0a9cf3c214044ac202178b193b9287
major <majorofffpavel@gmail.com>
2022-02-06 20:19:14 +0300

    add urls and templates

--- PasswordGenProject/urls.py
+++ PasswordGenProject/urls.py
@@ -1,7 +1,7 @@
-"""PasswordGenProject URL Configuration
+"""PasswordGenerator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from pswrdApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('password/', views.password, name='password'),
+    path('contacts/', views.contacts, name='contacts')
 ]
